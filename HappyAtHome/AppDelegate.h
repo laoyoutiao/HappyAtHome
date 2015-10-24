bc2d850fdde7ef4c04503c56f5d7b5a3bfb2ff64
laoyoutiao <573786543@qq.com>
2015-10-24 15:37:02 +0800

    Initial Commit

---
+++ HappyAtHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyAtHome
+//
+//  Created by 玉文辉 on 15/10/24.
+//  Copyright © 2015年 yuwenhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyAtHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HappyAtHome
+//
+//  Created by 玉文辉 on 15/10/24.
+//  Copyright © 2015年 yuwenhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
